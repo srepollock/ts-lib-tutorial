1014048f26d5301bad3e900afbb2ea868449e261
Spencer Pollock <spencer@spollock.ca>
2018-12-04 21:31:34 -0800

    Fixing issue #1

---
+++ dist/Math.js
@@ -0,0 +1,34 @@
+/**
+ * Adds two numbers together
+ * @param  {number} x
+ * @param  {number} y
+ * @returns number
+ */
+export function add(x, y) {
+    return x + y;
+}
+/**
+ * Subtracts Y from X.
+ * @param  {number} x
+ * @param  {number} y
+ * @returns number
+ */
+export function subtract(x, y) {
+    return x - y;
+}
+/**
+ * Squares a number.
+ * @param  {number} x
+ * @returns number
+ */
+export function square(x) {
+    return Math.pow(x, 2);
+}
+/**
+ * Cubes a number
+ * @param  {number} x
+ * @returns number
+ */
+export function cube(x) {
+    return Math.pow(x, 3);
+}
